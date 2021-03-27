87bdc2e748c45b092f0ac523c02baecc205077a9
Mayukh Basu <roxy.firebird@gmail.com>
2021-03-27 11:16:14 +0530

    added nginx

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post("/posts/:id/comments", async(req, res) => {
     const comments = commentsByPostID[req.params.id] || [];
     comments.push({id: commentID, content, status: 'pending'});
     commentsByPostID[req.params.id] = comments;
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentID,
@@ -39,7 +39,7 @@ app.post("/events", async(req, res) => {
         const comments = commentsByPostID[postId];
         const comment = comments.find(comment => comment.id === id);
         comment.status = status;
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentUpdated',
             data: {
                 id, status, postId, content
